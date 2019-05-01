f8c3780cd1b3f85ea974c7a5a721d49209c938c6
anga13 <kalle.spik@hotmail.com>
2019-05-01 11:34:29 +0200

    Add some controller tests

--- __tests__/controller.test.js
+++ __tests__/controller.test.js
@@ -1,7 +1,25 @@
 const controller = require('../controller')
 
+let db = require('../db')
+let stats = require('../stats')
+
+jest.mock('../db')
+jest.mock('../stats')
+
 describe('The getHours() function', () => {
-	test.todo('stuff')
+	test('stuff', () => {
+		db.getEmployeeTotalsInInterval.mockImplementation((from, to) => dbEmployees)
+		stats.calculateHours.mockImplementation(stamps => 9)
+
+		let staff = controller.getHours('2010-10-20','2017-12-02')
+		let fred = staff[0]
+		let barney = staff[1]
+		expect(staff.length).toBe(2)
+		expect(fred.name).toBe('Fred Flintstone')
+		expect(barney.name).toBe('Barney Rubble')
+		expect(fred.hours).toBe(9)
+		expect(barney.hours).toBe(9)
+	})
 })
 describe('The getTodaysStats() function', () => {
 	test.todo('stuff')
@@ -10,3 +28,32 @@ describe('The getPersonalStats function', () => {
 	test.todo('stuff')
 })
 
+const dbEmployees = [
+	{
+		id:1,
+		name: 'Fred Flintstone',
+		stamps: [
+		{
+			employeeId: 1,
+			checkedIn: true,
+			time: '2017-01-22 7:30'
+		},{
+			employeeId: 1,
+			checkedIn: false,
+			time: '2017-01-22 16:30'
+		}]
+	},
+	{
+		id:2,
+		name: 'Barney Rubble',
+		stamps: [{
+			employeeId: 2,
+			checkedIn: true,
+			time: '2017-01-22 8:30'
+		},{
+			employeeId: 2,
+			checkedIn: false,
+			time: '2017-01-22 13:30'
+		}]
+	}
+]
