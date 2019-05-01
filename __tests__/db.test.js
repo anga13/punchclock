cb15f43aabc03349add96f5b92da6569bbb69b20
gabeN <antonkal@kth.se>
2019-05-01 15:00:31 +0200

    Jest-tester för databasen.

--- __tests__/db.test.js
+++ __tests__/db.test.js
@@ -1,11 +1,47 @@
 const db = require('../db')
 
+
 describe('Employee DAO', () => {
-	test.todo('Returns all instances of employee')
-	test.todo('Returns the instance of employee with the given id')
+        db.getEmployees=jest.fn(()=>
+                [{id:0, name:"Fred Flintstone"},
+                 {id:1, name:"Barney Rubble"},
+                 {id:2, name:"Mister Kill"},
+                 {id:3, name:"Muscles McBoulder"}])
+
+        test('Returns all instances of employee', ()=>{
+                const emps=db.getEmployees()
+                expect(emps).toBeDefined()
+                expect(emps).toBeInstanceOf(Array)
+        })
+
+        test.each([0,1,2,3])('Returns the instance of employee with the given id', (a)=>{
+                const emp=db.getEmployeeById(a)
+                expect(emp).toBeDefined()
+                expect(emp.id).toBeDefined()
+                expect(emp.name).toBeDefined()
+        })
 })
 
 describe('Timestamp DAO', () => {
-	test.todo('Returns all timestamps in the given interval')
-	test.todo('Returns all timestamps with the given id')
+        db.getStamps=jest.fn(()=>[{id:0, in:true, time:9},
+                                  {id:0, in:false, time:11},
+                                  {id:0, in:true, time:13},
+                                  {id:1, in:true, time:8},
+                                  {id:1, in:false, time:11},
+                                  {id:1, in:true, time:13},
+                                  {id:2, in:true, time:9},
+                                  {id:2, in:false, time:12},
+                                  {id:3, in:true, time:8}])
+
+        test.each([[0,23],[8,11],[4,9]])('Returns all timestamps in the given interval', (a,b)=>{
+                const stamps=db.getStampsInInterval(a,b);
+                expect(stamps).toBeDefined()
+                expect(stamps).toBeInstanceOf(Array)
+        })
+        test.each([0,1,2,3])('Returns all timestamps with the given id', (a)=>{
+                const stamps=db.getStampsById(a)
+                expect(stamps).toBeDefined()
+                expect(stamps).toBeInstanceOf(Array)
+        })
 })
+
--- db.js
+++ db.js
@@ -1,7 +1,30 @@
-module.exports = {
-	getEmployees: (cb) => { },
-	getEmployeeById: (id, cb) => {},
-	getStampsInInterval: (from, to) => {},
-	getStampsById: (id) => {},
+const db={
+        getEmployeeTotalsInInterval: (from, to) => {},
+
+        getEmployees: () => { return undefined },
+
+        getEmployeeById: (id) => {
+                const all=db.getEmployees()
+                var returned=all.filter(function(employee) {
+                        return employee.id===id })
+                return returned[0]
+        },
+
+        getStamps: () => { return undefined; },
+
+        getStampsInInterval: (from, to) => {
+                const all=db.getStamps()
+                var returned=all.filter(function(stamp) {
+                        return (stamp.time>=from) || (stamp.time<=to) })
+                return returned
+        },
+
+        getStampsById: (id) => {
+                const all=db.getStamps()
+                var returned=all.filter(function(stamp) {
+                        return id===stamp.id })
+                return returned
+        },
 }
 
+module.exports=db
