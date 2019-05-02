aed0fc42a0c4f316d82dddf27880f98cf4ae16b7
anga13 <kalle.spik@hotmail.com>
2019-05-02 09:56:57 +0200

    Add tests for personal stats endpoint

--- __tests__/integration/app.test.js
+++ __tests__/integration/app.test.js
@@ -4,7 +4,19 @@ const app = require('../../app')
 const controller = require('../../controller')
 
 jest.mock('../../controller')
+
 controller.getHours.mockImplementation((from,to) => {})
+controller.getPersonalStats.mockImplementation((id) => {
+	let id_nr = parseInt(id, 10)
+	if(id_nr > 3 || id_nr < 1) {
+		throw 'Not in DB'
+	} else return {
+		id: id_nr,
+		name: 'Pebbles',
+		workedHours: 12
+	}
+})
+
 describe('HTTP interface endpoints', () => {
 	describe('GET /hours', () => {
 		test('returns error code with no query params', async () => {
@@ -16,7 +28,28 @@ describe('HTTP interface endpoints', () => {
 			expect(response.statusCode).toBe(200)
 		})
 	})
-	test.todo('GET /today')
-	test.todo('GET /employee/:id/stats')
+	describe('GET /today', () => {
+		test('returns with status code 200', async () => {
+			let response = await request(app).get('/today')
+			expect(response.statusCode).toBe(200)
+		})
+	})
+	describe('GET /employee/:id/stats', () => {
+		test.each([1, 2, 3])('returns with status code 200 for id %d', async (id) => {
+			let response = await request(app).get(`/employee/${id}/stats`)
+			expect(response.statusCode).toBe(200)
+		})
+		test.each([100,324,0])('returns with status code 404 for id %d', async (id) => {
+			let response = await request(app).get(`/employee/${id}/stats`)
+			expect(response.statusCode).toBe(404)
+		})
+		test.each([1, 2, 3])('returns stats for employee ¤%d', async (id) => {
+			let response = await request(app).get(`/employee/${id}/stats`)
+			let employee = response.body
+			expect(employee.id).toBe(id)
+			expect(employee.name).toBe('Pebbles')
+			expect(employee.workedHours).toBe(12)
+		})
+	})
 })
 
