419c9fcfdfbdafdc6a2e1cac7dca87148db39441
anga13 <kalle.spik@hotmail.com>
2019-09-04 14:19:54 +0200

    Use env variable for URL

--- __tests__/gui/homepage.test.js
+++ __tests__/gui/homepage.test.js
@@ -1,19 +1,19 @@
 const {Builder, By} = require('selenium-webdriver')
 const firefox = require('selenium-webdriver/firefox')
-
+const BASE_URL = process.env.APP_URL
 const driver = new Builder()
 	.forBrowser('firefox')
 	.setFirefoxOptions(new firefox.Options().headless())
 	.build()
 
 test('Startpage has app element', async () => {
-	driver.get('localhost:8080')
+	driver.get(`${BASE_URL}`)
 	await expect(driver.findElement(By.id('app'))
 		.getTagName()).resolves.toBe('div')
 })
 
 test('/today page lists all employees', async () => {
-	driver.get('localhost:8080/today')
+	driver.get(`${BASE_URL}/today`)
 	const elements = await driver.findElement(By.id('app'))
 		.findElements(By.className('employee'))
 	expect(elements).toHaveLength(3)
